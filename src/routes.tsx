566e7dba9d98b6d29632bc56386b25967c7f3a22
Rosele Silva <rosele.silva@ibm.com>
2024-01-12 08:16:09 -0300

    Criação dashboard

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,27 +5,35 @@ import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
 import { Orders } from './pages/app/orders/orders'
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/dashboard/dashboard'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
     children: [
-      { path: '/', element: <Dashboard /> },
-      { path: '/orders', element: <Orders /> }
-    ]
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: '/orders',
+        element: <Orders />,
+      },
+    ],
   },
   {
     path: '/',
     element: <AuthLayout />,
     children: [
-      { path: '/sign-in', element: <SignIn /> },
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
       {
         path: '/sign-up',
         element: <SignUp />,
       },
-
-    ]
-  }
+    ],
+  },
 ])
