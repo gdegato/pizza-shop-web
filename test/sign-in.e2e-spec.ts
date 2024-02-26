d112ebf6c27aac7f9efa1c22e68a828b69e1bd16
gdegato <rosele.tst@gmail.com>
2024-02-26 20:02:08 -0300

    Ajustes no arquivo sign-in-spec

--- test/sign-in.e2e-spec.ts
+++ test/sign-in.e2e-spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test('sign in successfully', async ({ page }) => {
     await page.goto('/sign-in', { waitUntil: 'networkidle' })
 
-    await page.getByLabel('Seu e-mail').fill('johndoe@example.com')
+    await page.getByLabel('Seu e-mail').fill('rosele.tst@gmail.com')
     await page.getByRole('button', { name: 'Acessar painel' }).click()
 
     const toast = page.getByText(
@@ -19,7 +19,7 @@ test('sign in with wrong credentials', async ({ page }) => {
     await page.getByLabel('Seu e-mail').fill('wrong@example.com')
     await page.getByRole('button', { name: 'Acessar painel' }).click()
 
-    const toast = page.getByText('Credenciais inválidas.')
+    const toast = page.getByText('Credenciais inválidas')
 
     await expect(toast).toBeVisible()
 })
